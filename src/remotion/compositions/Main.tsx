920e15b06c32a0c6806b87611131121b6fbd5b5b
Nicolas Coutureau <nicolas.coutureau@gmail.com>
2026-01-30 12:42:18 +0100

    Refactor import path for TextAnimation component
    - Updated the import statement for the TextAnimation component to reflect its new location in the project structure, enhancing code organization and maintainability.

--- src/remotion/compositions/Main.tsx
+++ src/remotion/compositions/Main.tsx
@@ -1,7 +1,7 @@
 import { AbsoluteFill, Artifact, useCurrentFrame } from "remotion";
 import gsap from "gsap";
 import { SplitText } from "gsap/SplitText";
-import { TextAnimation } from "../base/components/TextAnimation";
+import { TextAnimation } from "../base/components/text/TextAnimation";
 import { loadFont } from "@remotion/google-fonts/Inter";
 
 // This re-runs on every HMR update of this file
